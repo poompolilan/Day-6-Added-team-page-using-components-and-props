de2b31c9c092bda315cd36947d28ff7e1d4531b7
poompolilan <poompolilan.ug.23.it@francisxavier.ac.in>
2025-08-19 09:39:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ function App() {
   const teamMembers = [
     {
       id: 1,
-      name: "Alice Johnson",
-      role: "Frontend Developer",
-      photo: "https://randomuser.me/api/portraits/women/44.jpg",
+      name: "Virat kohli",
+      role: "Cricketer",
+      photo: "https://cdn.britannica.com/48/252748-050-C514EFDB/Virat-Kohli-India-celebrates-50th-century-Cricket-November-15-2023.jpg",
     },
     {
       id: 2,
-      name: "Bob Smith",
-      role: "Backend Developer",
-      photo: "https://randomuser.me/api/portraits/men/32.jpg",
+      name: "Neeraj Chopra",
+      role: "javelin thrower",
+      photo: "https://d16f573ilcot6q.cloudfront.net/wp-content/uploads/2024/08/Neeraj-Chopra-eyes-Diamond-League-final-javelin-throw-through-Lausanne-No-Arshad-Nadeem-scaled.webp",
     },
     {
       id: 3,
-      name: "Charlie Brown",
-      role: "UI/UX Designer",
-      photo: "https://randomuser.me/api/portraits/men/65.jpg",
+      name: "Messi",
+      role: "Football player",
+      photo: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt8a7df3ea3cb0fbec/637ca449c36f4b4b469855a3/Lionel_Messi(7).jpg?auto=webp&format=pjpg&width=3840&quality=60",
     },
     {
       id: 4,
-      name: "Diana Patel",
-      role: "QA Engineer",
-      photo: "https://randomuser.me/api/portraits/women/12.jpg",
+      name: "Ronaldo",
+      role: "Football player",
+      photo: "https://images2.thanhnien.vn/528068263637045248/2023/10/15/cristiano-ronaldo-16973598904331934094389.jpeg",
     },
   ];
 
@@ -50,4 +50,5 @@ function App() {
 // Mount React app
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(<App />);
